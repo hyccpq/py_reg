bc06d452b01200e72ef6115ad1860ef879fd20b8
hyccpq <hyccpq@hotmail.com>
2018-04-20 00:25:07 +0800

    test1

--- Article/Article/spiders/jobbole.py
+++ Article/Article/spiders/jobbole.py
@@ -5,7 +5,9 @@ import scrapy
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/113789/']
 
     def parse(self, response):
+        re_selector = response.xpath('//*[@id="post-113789"]/div[1]/h1/text()')
+        print(re_selector)
         pass
